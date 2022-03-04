9141804bc79573b1a9ae8abd149394177d36d1e7
Fridwen <91970497+Fridwen@users.noreply.github.com>
2022-03-04 20:23:10 +0900

    Update RuRoo.py

--- RuRoo.py
+++ RuRoo.py
@@ -67,12 +67,15 @@ async def on_message(message):
         embed.add_field(name="B조", value=teamB, inline=True)
         await message.channel.send(embed=embed)
     
-    if message.content == "!명령어":
+     if message.content == "!명령어":
         if message.author.dm_channel:
             embed = discord.Embed(title = "[루루봇 명령어]", color = discord.Color.dark_blue())
             embed.add_field(name = "**★ 주사위**", value = "- !주사위 (정수) - 1 부터 해당 정수의 범위로 랜덤한 값을 추출한다냥", inline = False)
             embed.add_field(name = "**★ 경기**", value = "- !경기 (이름) (이름) (이름) (이름)... - 짝수개의 값을 넣으면 랜덤하게 두 팀으로 나눠준다냥 ", inline = False)
             embed.add_field(name = "**★ 안녕**", value = "- !안녕 - 인사를 해준다냥", inline = False)
+            embed.add_field(name = "**★ 이리와**", value = "- !이리와 - 음성 채널에 들어온다냥", inline = False)
+            embed.add_field(name = "**★ 저리가**", value = "- !저리가 - 음성 채널에서 나간다냥", inline = False)
+            embed.add_field(name = "**★ 전적검색**", value = "- !전적검색 (이름) - (이름) 플레이어의 롤 전적을 검색해준다냥", inline = False)
             embed.set_author(name = "루루봇", icon_url = "https://cdn.discordapp.com/attachments/916751424686260276/916756875981238272/69e5983b355aff34.jpg")
             user_id ='686565060826366004'
             embed.add_field(name = "\n★ 문의", value = f"<@{user_id}>", inline = False)
@@ -84,10 +87,30 @@ async def on_message(message):
             embed.add_field(name = "**★ 주사위**", value = "- !주사위 (정수) - 1 부터 해당 정수의 범위로 랜덤한 값을 추출한다냥", inline = False)
             embed.add_field(name = "**★ 경기**", value = "- !경기 (이름) (이름) (이름) (이름)... - 짝수개의 값을 넣으면 랜덤하게 두 팀으로 나눠준다냥 ", inline = False)
             embed.add_field(name = "**★ 안녕**", value = "- !안녕 - 인사를 해준다냥", inline = False)
+            embed.add_field(name = "**★ 이리와**", value = "- !이리와 - 음성 채널에 들어온다냥", inline = False)
+            embed.add_field(name = "**★ 저리가**", value = "- !저리가 - 음성 채널에서 나간다냥", inline = False)
+            embed.add_field(name = "**★ 전적검색**", value = "- !전적검색 (이름) - (이름) 플레이어의 롤 전적을 검색해준다냥", inline = False)
             embed.set_author(name = "루루봇", icon_url = "https://cdn.discordapp.com/attachments/916751424686260276/916756875981238272/69e5983b355aff34.jpg")
             user_id ='686565060826366004'
             embed.add_field(name = "\n★ 문의", value = f"<@{user_id}>", inline = False)
             await channel.send(embed=embed)
+    
+    
+    if message.content.startswith("!이리와"):
+        user = message.author
+        vc = user.voice.channel
+        bot = client.get_user('916623193010229278')
+        voice= discord.utils.get(client.voice_clients, guild=message.guild)
+        if voice == None:
+            await vc.connect()
+            await message.channel.send("음성 채널에 들어왔다냥")
+
+    if message.content == "!저리가":
+        for voc in client.voice_clients:
+            if voc.guild == message.guild:
+                voice1 = voc
+        await voice1.disconnect()
+        await message.channel.send("음성 채널에서 나갔다냥")
     
     if message.content.startswith("!URL재생 "):
         user = message.author
@@ -108,22 +131,6 @@ async def on_message(message):
         else:
             await message.channel.send("노래가 이미 재생되고 있다냥")
 
-    
-    if message.content.startswith("!이리와"):
-        user = message.author
-        vc = user.voice.channel
-        bot = client.get_user('916623193010229278')
-        voice = discord.utils.get(client.voice_clients, guild=message.guild)
-        if voice == None:
-            await vc.connect()
-            await message.channel.send("음성 채널에 들어왔다냥")
-
-    if message.content == "!저리가":
-        for voc in client.voice_clients:
-            if voc.guild == message.guild:
-                voice1 = voc
-        await voice1.disconnect()
-        await message.channel.send("음성 채널에서 나갔다냥")
 
     if message.content.startswith("!전적검색 "):
         user = message.content.replace("!전적검색 ", "")
